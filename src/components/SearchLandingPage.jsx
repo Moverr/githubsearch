628e167394039fc749a388209d232744ff991c39
Muyinda ROgers <moverr@gmail.com>
2021-06-24 16:09:02 +0300

    Refactoring :

--- src/components/SearchLandingPage.jsx
+++ src/components/SearchLandingPage.jsx
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class SearchLandingPage extends Component {
 
     //search bar ui 
-    searchbar  =   (props) => {
-        return  <form className="form-inline col-md-12 ">
-        <input className="form-control col-md-9 " type="text" />
-        <button className="btn btn-primary my-2 my-sm-0" type="submit">
-             <i className="fa fa-search"></i>
-        </button>
-    </form>
+    searchbar = (props) => {
+        return <form className="form-inline col-md-12 ">
+            <input className="form-control col-md-9 " type="text" />
+            <button className="btn btn-primary my-2 my-sm-0" type="submit">
+                <i className="fa fa-search"></i>
+            </button>
+        </form>
     }
 
     render() {
@@ -18,12 +18,10 @@ class SearchLandingPage extends Component {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-md-12">
-                            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-
-                                {/* <!-- Add Github icon --> */}
+                            <nav className="navbar navbar-expand-lg navbar-light bg-light"> 
                                 <a className="navbar-brand docs-creator" href="#"><i className="fa fa-github"></i></a>
                                 <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                                   <this.searchbar/>
+                                    <this.searchbar />
 
                                 </div>
                             </nav>
