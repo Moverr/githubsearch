9098895122799cd75724ca6af4941f0122da88e0
Muyinda ROgers <moverr@gmail.com>
2021-06-25 00:01:45 +0300

    Debuggginng

--- src/components/SearchLandingPage.jsx
+++ src/components/SearchLandingPage.jsx
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
+import SearchForm from './SearchForm';
 
 class SearchLandingPage extends Component {
 
     //search bar ui 
-    searchbar = (props) => {
-        return <form className="form-inline col-md-12 ">
-            <input className="form-control col-md-9 " placeholder={props.placeholder} type="text" />
-            <button className="btn btn-primary my-2 my-sm-0" type="submit">
-                <i className="fa fa-search"></i>
-            </button>
-        </form>
-    }
-
+  
     render() {
         return (
             <div>
@@ -21,7 +14,8 @@ class SearchLandingPage extends Component {
                             <nav className="navbar navbar-expand-lg navbar-light bg-light"> 
                                 <a className="navbar-brand docs-creator" href="#"><i className="fa fa-github"></i></a>
                                 <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                                    <this.searchbar placeholder="Search " age="232" />
+                                    {/* //todo: Search Form  */}
+                                    <SearchForm placeholder="Search " age="232" />
 
                                 </div>
                             </nav>
