a004de6f9341e31662d1b5a6bf9c14067324027f
Muyinda ROgers <moverr@gmail.com>
2021-06-24 16:13:47 +0300

    DEbuggging

--- src/components/SearchLandingPage.jsx
+++ src/components/SearchLandingPage.jsx
@@ -5,7 +5,7 @@ class SearchLandingPage extends Component {
     //search bar ui 
     searchbar = (props) => {
         return <form className="form-inline col-md-12 ">
-            <input className="form-control col-md-9 " type="text" />
+            <input className="form-control col-md-9 " placeholder={props.placeholder} type="text" />
             <button className="btn btn-primary my-2 my-sm-0" type="submit">
                 <i className="fa fa-search"></i>
             </button>
@@ -21,12 +21,15 @@ class SearchLandingPage extends Component {
                             <nav className="navbar navbar-expand-lg navbar-light bg-light"> 
                                 <a className="navbar-brand docs-creator" href="#"><i className="fa fa-github"></i></a>
                                 <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                                    <this.searchbar />
+                                    <this.searchbar placeholder="Search " age="232" />
 
                                 </div>
                             </nav>
                         </div>
                     </div>
+                    <div className="row">
+                        Interesting Scenario
+                   </div>
                 </div>
             </div>
         );
