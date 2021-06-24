daabe3cd2d4994c8e39d2599cccce0e5479f4bc7
Muyinda ROgers <moverr@gmail.com>
2021-06-24 11:39:19 +0300

    initial deployment

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SearchLandingPage from './components/SearchLandingPage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SearchLandingPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
