4a1c5b7876e0f3b085f46fde8b8bd13a373b619f
Muyinda ROgers <moverr@gmail.com>
2021-06-25 16:40:30 +0300

    Introducing lazy loading of image

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const ProfileCard = function(props){
     return   <div className="profileCard">  
 
-    <div className="avatar"> </div>
+    <div className="avatar"> 
+     
+    </div>
+    <div className="username"></div>
+    <div className="username"></div>
+    <div className="username"></div>
+    
     </div>
 }
 export default ProfileCard;
