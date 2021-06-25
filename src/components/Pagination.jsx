dfbc8235f13b78ade7a6f4402fffd219d0382e52
Muyinda ROgers <moverr@gmail.com>
2021-06-25 15:54:26 +0300

    DEbugging

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import ButtonField from './formelements/ButtonField';
 
-const Pagination = function(props){
-    return     <nav>
-    <ul className="pagination">
-        <li className="page-item">
-            <ButtonField className="page-link docs-creator" href="#"><i className="fa fa-backward"></i></ButtonField>
-        </li>
-        <li className="page-item">
-            <ButtonField class="page-link docs-creator" href="#">1</ButtonField>
-        </li>
-        <li className="page-item">
-            <button className="page-link docs-creator" href="#">2</button>
-        </li>
-        <li className="page-item">
-            <ButtonField className="page-link docs-creator" href="#">3</ButtonField>
-        </li>
-        <li className="page-item">
-            <ButtonField className="page-link docs-creator" href="#">4</ButtonField>
-        </li>
-        <li className="page-item">
-            <ButtonField className="page-link docs-creator" href="#">5</ButtonField>
-        </li>
-        <li className="page-item">
-            <ButtonField className="page-link docs-creator" href="#"><i className="fa fa-forward"></i></ButtonField>
-        </li>
-    </ul>
-</nav>;
+const Pagination = function (props) {
+    return <nav>
+        <ul className="pagination">
+            <li className="page-item">
+                <ButtonField className="page-link docs-creator" href="#"><i className="fa fa-backward"></i></ButtonField>
+            </li>
+            <li className="page-item">
+                <ButtonField class="page-link docs-creator" href="#">1</ButtonField>
+            </li>
+            <li className="page-item">
+                <button className="page-link docs-creator" href="#">2</button>
+            </li>
+            <li className="page-item">
+                <ButtonField className="page-link docs-creator" href="#">3</ButtonField>
+            </li>
+            <li className="page-item">
+                <ButtonField className="page-link docs-creator" href="#">4</ButtonField>
+            </li>
+            <li className="page-item">
+                <ButtonField className="page-link docs-creator" href="#">5</ButtonField>
+            </li>
+            <li className="page-item">
+                <ButtonField className="page-link docs-creator" href="#"><i className="fa fa-forward"></i></ButtonField>
+            </li>
+        </ul>
+    </nav>;
 }
 
 
--- src/components/SearchLandingPage.jsx
+++ src/components/SearchLandingPage.jsx
@@ -7,7 +7,140 @@ import Pagination from './Pagination';
 
 class SearchLandingPage extends Component {
 
-    //todo: render elements 
+
+   
+    
+    constructor(props){
+        super(props);
+        const profiles = this.getProfils();
+        console.log(profiles);
+        this.state = {profiles:this.profiles}
+    }
+    
+      getProfils = () =>{
+        return {
+            "total_count": 427409,
+            "incomplete_results": false,
+            "items": [
+                {
+                    "login": "ju",
+                    "id": 67181870,
+                    "node_id": "MDQ6VXNlcjY3MTgxODcw",
+                    "avatar_url": "https://avatars.githubusercontent.com/u/67181870?v=4",
+                    "gravatar_id": "",
+                    "url": "https://api.github.com/users/ju",
+                    "html_url": "https://github.com/ju",
+                    "followers_url": "https://api.github.com/users/ju/followers",
+                    "following_url": "https://api.github.com/users/ju/following{/other_user}",
+                    "gists_url": "https://api.github.com/users/ju/gists{/gist_id}",
+                    "starred_url": "https://api.github.com/users/ju/starred{/owner}{/repo}",
+                    "subscriptions_url": "https://api.github.com/users/ju/subscriptions",
+                    "organizations_url": "https://api.github.com/users/ju/orgs",
+                    "repos_url": "https://api.github.com/users/ju/repos",
+                    "events_url": "https://api.github.com/users/ju/events{/privacy}",
+                    "received_events_url": "https://api.github.com/users/ju/received_events",
+                    "type": "User",
+                    "site_admin": false,
+                    "score": 1.0
+                },
+                {
+                    "login": "soasme",
+                    "id": 369081,
+                    "node_id": "MDQ6VXNlcjM2OTA4MQ==",
+                    "avatar_url": "https://avatars.githubusercontent.com/u/369081?v=4",
+                    "gravatar_id": "",
+                    "url": "https://api.github.com/users/soasme",
+                    "html_url": "https://github.com/soasme",
+                    "followers_url": "https://api.github.com/users/soasme/followers",
+                    "following_url": "https://api.github.com/users/soasme/following{/other_user}",
+                    "gists_url": "https://api.github.com/users/soasme/gists{/gist_id}",
+                    "starred_url": "https://api.github.com/users/soasme/starred{/owner}{/repo}",
+                    "subscriptions_url": "https://api.github.com/users/soasme/subscriptions",
+                    "organizations_url": "https://api.github.com/users/soasme/orgs",
+                    "repos_url": "https://api.github.com/users/soasme/repos",
+                    "events_url": "https://api.github.com/users/soasme/events{/privacy}",
+                    "received_events_url": "https://api.github.com/users/soasme/received_events",
+                    "type": "User",
+                    "site_admin": false,
+                    "score": 1.0
+                },
+                {
+                    "login": "huo-ju",
+                    "id": 561810,
+                    "node_id": "MDQ6VXNlcjU2MTgxMA==",
+                    "avatar_url": "https://avatars.githubusercontent.com/u/561810?v=4",
+                    "gravatar_id": "",
+                    "url": "https://api.github.com/users/huo-ju",
+                    "html_url": "https://github.com/huo-ju",
+                    "followers_url": "https://api.github.com/users/huo-ju/followers",
+                    "following_url": "https://api.github.com/users/huo-ju/following{/other_user}",
+                    "gists_url": "https://api.github.com/users/huo-ju/gists{/gist_id}",
+                    "starred_url": "https://api.github.com/users/huo-ju/starred{/owner}{/repo}",
+                    "subscriptions_url": "https://api.github.com/users/huo-ju/subscriptions",
+                    "organizations_url": "https://api.github.com/users/huo-ju/orgs",
+                    "repos_url": "https://api.github.com/users/huo-ju/repos",
+                    "events_url": "https://api.github.com/users/huo-ju/events{/privacy}",
+                    "received_events_url": "https://api.github.com/users/huo-ju/received_events",
+                    "type": "User",
+                    "site_admin": false,
+                    "score": 1.0
+                },
+                {
+                    "login": "Arkham",
+                    "id": 62895,
+                    "node_id": "MDQ6VXNlcjYyODk1",
+                    "avatar_url": "https://avatars.githubusercontent.com/u/62895?v=4",
+                    "gravatar_id": "",
+                    "url": "https://api.github.com/users/Arkham",
+                    "html_url": "https://github.com/Arkham",
+                    "followers_url": "https://api.github.com/users/Arkham/followers",
+                    "following_url": "https://api.github.com/users/Arkham/following{/other_user}",
+                    "gists_url": "https://api.github.com/users/Arkham/gists{/gist_id}",
+                    "starred_url": "https://api.github.com/users/Arkham/starred{/owner}{/repo}",
+                    "subscriptions_url": "https://api.github.com/users/Arkham/subscriptions",
+                    "organizations_url": "https://api.github.com/users/Arkham/orgs",
+                    "repos_url": "https://api.github.com/users/Arkham/repos",
+                    "events_url": "https://api.github.com/users/Arkham/events{/privacy}",
+                    "received_events_url": "https://api.github.com/users/Arkham/received_events",
+                    "type": "User",
+                    "site_admin": false,
+                    "score": 1.0
+                },
+                {
+                    "login": "0oneo",
+                    "id": 1052534,
+                    "node_id": "MDQ6VXNlcjEwNTI1MzQ=",
+                    "avatar_url": "https://avatars.githubusercontent.com/u/1052534?v=4",
+                    "gravatar_id": "",
+                    "url": "https://api.github.com/users/0oneo",
+                    "html_url": "https://github.com/0oneo",
+                    "followers_url": "https://api.github.com/users/0oneo/followers",
+                    "following_url": "https://api.github.com/users/0oneo/following{/other_user}",
+                    "gists_url": "https://api.github.com/users/0oneo/gists{/gist_id}",
+                    "starred_url": "https://api.github.com/users/0oneo/starred{/owner}{/repo}",
+                    "subscriptions_url": "https://api.github.com/users/0oneo/subscriptions",
+                    "organizations_url": "https://api.github.com/users/0oneo/orgs",
+                    "repos_url": "https://api.github.com/users/0oneo/repos",
+                    "events_url": "https://api.github.com/users/0oneo/events{/privacy}",
+                    "received_events_url": "https://api.github.com/users/0oneo/received_events",
+                    "type": "User",
+                    "site_admin": false,
+                    "score": 1.0
+                }
+            ]
+        };
+    }
+
+
+    //profile cards 
+    profileCards =  function (props){
+        const profiles = props.items;
+        const profileCards = profiles.map((profile_item)=>
+        <ProfileCard />
+        );
+        
+        return profileCards;
+    }
 
     render() {
         return (
@@ -31,8 +164,11 @@ class SearchLandingPage extends Component {
                     <div className="row ">
                         <div className="col-md-8 main_window">
 
+                    {
+                        this.profileCards(this.state.profiles)
+                    }
                             {/* //todo: profile card  */}
-                            <ProfileCard />
+                           
                             <ProfileCard />
                             <ProfileCard />
                             <ProfileCard />
