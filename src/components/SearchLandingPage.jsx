9cf81b4ab13d42b1ccb51846edb0b9d8d1c1785c
Muyinda ROgers <moverr@gmail.com>
2021-06-25 13:02:12 +0300

    Debuggging

--- src/components/SearchLandingPage.jsx
+++ src/components/SearchLandingPage.jsx
@@ -7,46 +7,46 @@ import Pagination from './Pagination';
 
 class SearchLandingPage extends Component {
 
-    //search bar ui 
-  
+    //todo: render elements 
+
     render() {
         return (
             <div>
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-md-12">
-                            <nav className="navbar navbar-expand-lg navbar-light bg-light"> 
+                            <nav className="navbar navbar-expand-lg navbar-light bg-light">
                                 <span className="navbar-brand docs-creator" href="#"><i className="fa fa-github"></i></span>
                                 <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                                     {/* //todo: Search Form  */}
-                                    <SearchForm placeholder="Search " age="232" />
+                                    <SearchForm placeholder="Search" />
 
                                 </div>
                             </nav>
                         </div>
                     </div>
-                
 
-                {/* main window  */}
+
+                    {/* main window  */}
                     <div className="row ">
-				<div className="col-md-8 main_window">
-                 
-                 {/* //todo: profile card  */}
-                  <ProfileCard />
-                  <ProfileCard />
-                  <ProfileCard />
-                  <ProfileCard />
-                  <ProfileCard />
- 
+                        <div className="col-md-8 main_window">
 
+                            {/* //todo: profile card  */}
+                            <ProfileCard />
+                            <ProfileCard />
+                            <ProfileCard />
+                            <ProfileCard />
+                            <ProfileCard />
+
+
+                        </div>
+                        <div className="col-md-4">
+                            Detail
 				</div>
-				<div className="col-md-4">
-                    Detail
-				</div>
-			</div>
-            <div className="row">
-             <Pagination />
-            </div>
+                    </div>
+                    <div className="row">
+                        <Pagination />
+                    </div>
                 </div>
             </div>
         );
