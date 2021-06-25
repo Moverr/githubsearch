13b556bd1c5580ca2ef7d6c66bba34d6f6c2808d
Muyinda ROgers <moverr@gmail.com>
2021-06-26 01:40:47 +0300

    Debuggging

---
+++ src/components/DetailsView.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ButtonField from './formelements/ButtonField';
+import Avatar from './utils/Avatar';
+
+const DetailsView = function (props) {
+
+    const profiledetail = props.profiledetail;
+   console.log(props)
+    return <div className="profileCard">
+                <Avatar image={profiledetail.avatar_url} />
+                       <span><strong>Name :</strong> {profiledetail.name} </span><br/>
+                       <span><strong>Company :</strong> {profiledetail.company} </span><br/>
+
+                       <span><strong>Email :</strong> {profiledetail.email} </span><br/>
+
+                       <span><strong>bio :</strong> {profiledetail.bio} </span><br/>
+
+                       <span><strong>followers :</strong> {profiledetail.followers} </span><br/>
+
+                       <span><strong>following :</strong> {profiledetail.following} </span><br/>
+
+                       <span><strong>public repos :</strong> {profiledetail.public_repos} </span><br/>
+
+                       <span><strong>public gists : </strong>{profiledetail.public_gists} </span><br/>
+
+    </div>
+}
+export default DetailsView;
--- src/components/SearchLandingPage.jsx
+++ src/components/SearchLandingPage.jsx
@@ -6,6 +6,7 @@ import '../css/main.css';
 import Pagination from './Pagination';
 import Summary from './Summary';
 import Avatar from './utils/Avatar';
+import DetailsView from './DetailsView';
 
 class SearchLandingPage extends Component {
 
@@ -13,42 +14,43 @@ class SearchLandingPage extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { profiles: null
-            ,summary:null
-        ,profiledetail:{
-            "login": "Moverr",
-            "id": 8292044,
-            "node_id": "MDQ6VXNlcjgyOTIwNDQ=",
-            "avatar_url": "https://avatars.githubusercontent.com/u/8292044?v=4",
-            "gravatar_id": "",
-            "url": "https://api.github.com/users/Moverr",
-            "html_url": "https://github.com/Moverr",
-            "followers_url": "https://api.github.com/users/Moverr/followers",
-            "following_url": "https://api.github.com/users/Moverr/following{/other_user}",
-            "gists_url": "https://api.github.com/users/Moverr/gists{/gist_id}",
-            "starred_url": "https://api.github.com/users/Moverr/starred{/owner}{/repo}",
-            "subscriptions_url": "https://api.github.com/users/Moverr/subscriptions",
-            "organizations_url": "https://api.github.com/users/Moverr/orgs",
-            "repos_url": "https://api.github.com/users/Moverr/repos",
-            "events_url": "https://api.github.com/users/Moverr/events{/privacy}",
-            "received_events_url": "https://api.github.com/users/Moverr/received_events",
-            "type": "User",
-            "site_admin": false,
-            "name": "Muyinda Rogers ",
-            "company": null,
-            "blog": "https://moverr.github.io/",
-            "location": "Uganda, Kampala",
-            "email": null,
-            "hireable": true,
-            "bio": "Interested in Functional Programming, Big Data and Artificial Intelligence.\r\n",
-            "twitter_username": "MuyindaRogers4",
-            "public_repos": 188,
-            "public_gists": 3,
-            "followers": 42,
-            "following": 115,
-            "created_at": "2014-07-28T17:50:59Z",
-            "updated_at": "2021-06-25T09:17:14Z"
-          }
+        this.state = {
+            profiles: null
+            , summary: null
+            , profiledetail: {
+                "login": "Moverr",
+                "id": 8292044,
+                "node_id": "MDQ6VXNlcjgyOTIwNDQ=",
+                "avatar_url": "https://avatars.githubusercontent.com/u/8292044?v=4",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/Moverr",
+                "html_url": "https://github.com/Moverr",
+                "followers_url": "https://api.github.com/users/Moverr/followers",
+                "following_url": "https://api.github.com/users/Moverr/following{/other_user}",
+                "gists_url": "https://api.github.com/users/Moverr/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/Moverr/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/Moverr/subscriptions",
+                "organizations_url": "https://api.github.com/users/Moverr/orgs",
+                "repos_url": "https://api.github.com/users/Moverr/repos",
+                "events_url": "https://api.github.com/users/Moverr/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/Moverr/received_events",
+                "type": "User",
+                "site_admin": false,
+                "name": "Muyinda Rogers ",
+                "company": null,
+                "blog": "https://moverr.github.io/",
+                "location": "Uganda, Kampala",
+                "email": null,
+                "hireable": true,
+                "bio": "Interested in Functional Programming, Big Data and Artificial Intelligence.\r\n",
+                "twitter_username": "MuyindaRogers4",
+                "public_repos": 188,
+                "public_gists": 3,
+                "followers": 42,
+                "following": 115,
+                "created_at": "2014-07-28T17:50:59Z",
+                "updated_at": "2021-06-25T09:17:14Z"
+            }
         }
         this.profileCards = this.profileCards.bind(this);
         this.getProfileData = this.getProfileData.bind(this);
@@ -59,7 +61,7 @@ class SearchLandingPage extends Component {
 
     //get data
     getProfileData() {
-        
+
         return {
             "total_count": 427409,
             "incomplete_results": false,
@@ -173,18 +175,18 @@ class SearchLandingPage extends Component {
         };
     }
 
- getsummary(){
+    getsummary() {
         const elems = this.getProfileData();
-        const result = [elems.items.length,elems.total_count];
+        const result = [elems.items.length, elems.total_count];
         return result;
- }
- handleProfileDetail(profileitem){
+    }
+    handleProfileDetail(profileitem) {
 
- }
+    }
     //populate  cards 
     profileCards(props) {
         const elems = this.getProfileData();
-       
+
         const profiles = elems.items;
         const profileCards = profiles.map((profile_item) => <ProfileCard callback={this.handleProfileDetail} key={profile_item.id} profile={profile_item} />);
         return profileCards;
@@ -210,19 +212,21 @@ class SearchLandingPage extends Component {
 
                     <div className="row ">
                         {/* //summary represetnation  */}
-                    <Summary  summary={summary}/>
+                        <Summary summary={summary} />
                     </div>
                     {/* main window  */}
                     <div className="row ">
-                   
+
 
                         <div className="col-md-8 main_window">
                             {this.profileCards()}
                         </div>
 
                         <div className="col-md-4">
-                      <Avatar image={this.state.profiledetail.avatar_url} />
-				</div>
+
+                            <DetailsView className="col-md-4" profiledetail={this.state.profiledetail} />
+
+                        </div>
                     </div>
                     <div className="row">
                         <Pagination />
