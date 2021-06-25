d2128c054221f9e6b545dd8e3c0f3d9f5993bba1
Muyinda ROgers <moverr@gmail.com>
2021-06-25 16:29:32 +0300

    Debuggging

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 const ProfileCard = function(props){
     return   <div className="profileCard">  
+
+    <div className="avatar"> </div>
     </div>
 }
 export default ProfileCard;
