680b31936e7ad877372167608781edcf04036160
Muyinda ROgers <moverr@gmail.com>
2021-06-25 12:36:43 +0300

    Click Handler

--- src/components/formelements/tests/ButtonField.test.js
+++ src/components/formelements/tests/ButtonField.test.js
@@ -6,6 +6,7 @@ import Buttonfield from '../ButtonField';
  
 
 
+ 
 describe('Testing Input FIeld', () => {
   it('should  render correctly', () => {    
     const props = { };
@@ -13,6 +14,23 @@ describe('Testing Input FIeld', () => {
       const wrapper = shallow(<Buttonfield props />);
       expect(wrapper).toMatchSnapshot();
   });
+
+  it(' should respond to click event ',() =>{
+      const mockCallBAck = jest.fn();
+      const props = {
+        title:"Button Title",
+        name:'Button Name',
+        message:'Button Message',
+        className:'buttonField',
+        id:"buttonID",
+        callback:mockCallBAck()
+    };
+      const button = shallow(<Buttonfield props ><i>movers</i></Buttonfield>);
+      button.find('button').simulate('click'); 
+     expect(mockCallBAck).toHaveBeenCalled();      
+     expect(mockCallBAck).toHaveBeenCalledTimes(1);
+});
+
 })
 
 
