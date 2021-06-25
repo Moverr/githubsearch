283b15ca92b92629fafd73f5764f1ddccf80a490
Muyinda ROgers <moverr@gmail.com>
2021-06-25 20:26:34 +0300

    Debuggging

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import ButtonField from './formelements/ButtonField';
 
-const ProfileCard = function(props){
+const ProfileCard = function (props) {
 
     const profile = props.profile;
     console.log(props.profile);
-    return   <div className="profileCard">  
+    return <div className="profileCard">
 
-    <div className="avatar">    
-     <img src={profile.avatar_url} />
-    </div>
-    <div className="username">
-        {profile.login}
-    </div>
-    <div className="followers">1233</div>
-  <ButtonField > VIEW MORE</ButtonField>
-    
+        <div className="avatar">
+            <img src={profile.avatar_url} />
+        </div>
+        <div className="username">
+            {profile.login}
+        </div>
+        <div className="followers">1233</div>
+        <ButtonField > VIEW MORE</ButtonField>
+
     </div>
 }
 export default ProfileCard;
