fec03c161d0e3382b6f48874e918de6bc594e714
Muyinda ROgers <moverr@gmail.com>
2021-06-25 12:58:30 +0300

    Debuggging

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import ButtonField from './formelements/ButtonField';
 import InputField from './formelements/InputField';
 
 const SearchForm = (props) => {
     return <form className="form-inline col-md-12 ">
         <div className="input-group col-md-9">
             <InputField className="form-control col-md-9 " placeholder={props.placeholder} type="text" />
-            <button className="btn btn-primary my-2 my-sm-0" type="submit">
+            <ButtonField className="btn btn-primary my-2 my-sm-0" type="submit">
                 <i className="fa fa-search"></i>
-            </button>
+            </ButtonField>
         </div>
     </form>
 }
