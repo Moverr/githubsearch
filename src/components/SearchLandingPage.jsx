01d3e89435113533090c630d46c08e201f2e7179
Muyinda ROgers <moverr@gmail.com>
2021-06-26 15:17:07 +0300

    Debuggging : Summary

--- src/components/SearchLandingPage.jsx
+++ src/components/SearchLandingPage.jsx
@@ -21,9 +21,9 @@ class SearchLandingPage extends Component {
         super(props);
 
         this.state = {
-            displayview: "tabular"
+            displayview: "date"
             , profiles: []
-            , summary: null
+            , summary: [0,0]
             , profiledetail: {
                 "login": "Moverr",
                 "id": 8292044,
@@ -75,7 +75,7 @@ class SearchLandingPage extends Component {
       
      
         //fetch data 
-        const url  = 'https://api.github.com/search/users?q="ju"&page=1&per_page=5';
+        const url  = 'https://api.github.com/search/users?q="mov"&page=1&per_page=100';
         let promise =   axios.get(`${url}`,{
             headers:{
                 'Accept': 'application/vnd.github.v3+json',
@@ -84,9 +84,14 @@ class SearchLandingPage extends Component {
         });
   
         promise.then((response)=>{
-            console.log("Lord is merciful");
-            console.log(response.data);
-          //  this.setState({profiles:response.data.items});
+            
+            const total_count = response.data.total_count
+            const result_count = response.data.items.length;
+            const summary = [result_count,total_count];
+            
+             this.setState(
+                 {profiles:response.data,summary:summary}             
+                 );
         })
      
         
@@ -96,126 +101,14 @@ class SearchLandingPage extends Component {
     getProfileData() {
 
         return this.state.profiles;
-        /*
-        return {
-            "total_count": 427409,
-            "incomplete_results": false,
-            "items": [
-                {
-                    "login": "ju",
-                    "id": 67181870,
-                    "node_id": "MDQ6VXNlcjY3MTgxODcw",
-                    "avatar_url": "https://avatars.githubusercontent.com/u/67181870?v=4",
-                    "gravatar_id": "",
-                    "url": "https://api.github.com/users/ju",
-                    "html_url": "https://github.com/ju",
-                    "followers_url": "https://api.github.com/users/ju/followers",
-                    "following_url": "https://api.github.com/users/ju/following{/other_user}",
-                    "gists_url": "https://api.github.com/users/ju/gists{/gist_id}",
-                    "starred_url": "https://api.github.com/users/ju/starred{/owner}{/repo}",
-                    "subscriptions_url": "https://api.github.com/users/ju/subscriptions",
-                    "organizations_url": "https://api.github.com/users/ju/orgs",
-                    "repos_url": "https://api.github.com/users/ju/repos",
-                    "events_url": "https://api.github.com/users/ju/events{/privacy}",
-                    "received_events_url": "https://api.github.com/users/ju/received_events",
-                    "type": "User",
-                    "site_admin": false,
-                    "score": 1.0
-                },
-                {
-                    "login": "soasme",
-                    "id": 369081,
-                    "node_id": "MDQ6VXNlcjM2OTA4MQ==",
-                    "avatar_url": "https://avatars.githubusercontent.com/u/369081?v=4",
-                    "gravatar_id": "",
-                    "url": "https://api.github.com/users/soasme",
-                    "html_url": "https://github.com/soasme",
-                    "followers_url": "https://api.github.com/users/soasme/followers",
-                    "following_url": "https://api.github.com/users/soasme/following{/other_user}",
-                    "gists_url": "https://api.github.com/users/soasme/gists{/gist_id}",
-                    "starred_url": "https://api.github.com/users/soasme/starred{/owner}{/repo}",
-                    "subscriptions_url": "https://api.github.com/users/soasme/subscriptions",
-                    "organizations_url": "https://api.github.com/users/soasme/orgs",
-                    "repos_url": "https://api.github.com/users/soasme/repos",
-                    "events_url": "https://api.github.com/users/soasme/events{/privacy}",
-                    "received_events_url": "https://api.github.com/users/soasme/received_events",
-                    "type": "User",
-                    "site_admin": false,
-                    "score": 1.0
-                },
-                {
-                    "login": "huo-ju",
-                    "id": 561810,
-                    "node_id": "MDQ6VXNlcjU2MTgxMA==",
-                    "avatar_url": "https://avatars.githubusercontent.com/u/561810?v=4",
-                    "gravatar_id": "",
-                    "url": "https://api.github.com/users/huo-ju",
-                    "html_url": "https://github.com/huo-ju",
-                    "followers_url": "https://api.github.com/users/huo-ju/followers",
-                    "following_url": "https://api.github.com/users/huo-ju/following{/other_user}",
-                    "gists_url": "https://api.github.com/users/huo-ju/gists{/gist_id}",
-                    "starred_url": "https://api.github.com/users/huo-ju/starred{/owner}{/repo}",
-                    "subscriptions_url": "https://api.github.com/users/huo-ju/subscriptions",
-                    "organizations_url": "https://api.github.com/users/huo-ju/orgs",
-                    "repos_url": "https://api.github.com/users/huo-ju/repos",
-                    "events_url": "https://api.github.com/users/huo-ju/events{/privacy}",
-                    "received_events_url": "https://api.github.com/users/huo-ju/received_events",
-                    "type": "User",
-                    "site_admin": false,
-                    "score": 1.0
-                },
-                {
-                    "login": "Arkham",
-                    "id": 62895,
-                    "node_id": "MDQ6VXNlcjYyODk1",
-                    "avatar_url": "https://avatars.githubusercontent.com/u/62895?v=4",
-                    "gravatar_id": "",
-                    "url": "https://api.github.com/users/Arkham",
-                    "html_url": "https://github.com/Arkham",
-                    "followers_url": "https://api.github.com/users/Arkham/followers",
-                    "following_url": "https://api.github.com/users/Arkham/following{/other_user}",
-                    "gists_url": "https://api.github.com/users/Arkham/gists{/gist_id}",
-                    "starred_url": "https://api.github.com/users/Arkham/starred{/owner}{/repo}",
-                    "subscriptions_url": "https://api.github.com/users/Arkham/subscriptions",
-                    "organizations_url": "https://api.github.com/users/Arkham/orgs",
-                    "repos_url": "https://api.github.com/users/Arkham/repos",
-                    "events_url": "https://api.github.com/users/Arkham/events{/privacy}",
-                    "received_events_url": "https://api.github.com/users/Arkham/received_events",
-                    "type": "User",
-                    "site_admin": false,
-                    "score": 1.0
-                },
-                {
-                    "login": "0oneo",
-                    "id": 1052534,
-                    "node_id": "MDQ6VXNlcjEwNTI1MzQ=",
-                    "avatar_url": "https://avatars.githubusercontent.com/u/1052534?v=4",
-                    "gravatar_id": "",
-                    "url": "https://api.github.com/users/0oneo",
-                    "html_url": "https://github.com/0oneo",
-                    "followers_url": "https://api.github.com/users/0oneo/followers",
-                    "following_url": "https://api.github.com/users/0oneo/following{/other_user}",
-                    "gists_url": "https://api.github.com/users/0oneo/gists{/gist_id}",
-                    "starred_url": "https://api.github.com/users/0oneo/starred{/owner}{/repo}",
-                    "subscriptions_url": "https://api.github.com/users/0oneo/subscriptions",
-                    "organizations_url": "https://api.github.com/users/0oneo/orgs",
-                    "repos_url": "https://api.github.com/users/0oneo/repos",
-                    "events_url": "https://api.github.com/users/0oneo/events{/privacy}",
-                    "received_events_url": "https://api.github.com/users/0oneo/received_events",
-                    "type": "User",
-                    "site_admin": false,
-                    "score": 1.0
-                }
-            ]
-        };
-    
-    */
+       
     }
 
     getsummary() {
-        const elems = this.getProfileData();
-        const result = [0,0];
-        // [elems.items.length, elems.total_count];
+        const elems =  this.state.profiles;
+        console.log("passers passer ")
+        console.log(elems);
+        const result = [0,0];// [elems.items.length, elems.total_count];
         return result;
     }
     handleProfileDetail(profileitem) {
@@ -278,7 +171,7 @@ class SearchLandingPage extends Component {
 
 
     render() {
-        const summary = this.getsummary();
+        const summary = this.state.summary
 
         return (
             <div>
