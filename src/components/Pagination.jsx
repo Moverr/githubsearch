0777554c3e48772efafc707cacb80143b5139626
Muyinda ROgers <moverr@gmail.com>
2021-06-26 20:13:48 +0300

    Debugging

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 import ButtonField from './formelements/ButtonField';
 
+const computelist = function (prop) {
+    const page = 1;
+    const total_pages = 100;
+    let items = []
+    let next_items = 2;
+    if (page == 1) {
+        for (let i = page; i <= next_items + page; i++) {
+            items.push(<li>${i}</li>);
+        }
+        return items;
+    }
+    for (let i = page - next_items; i <= next_items + page; i++) {
+        items.push(<li>${i}</li>);
+    }
+
+    return items;
+}
+
 const Pagination = function (props) {
     return <nav>
         <ul className="pagination">
@@ -11,7 +29,7 @@ const Pagination = function (props) {
                 <ButtonField className="page-link docs-creator" href="#">1</ButtonField>
             </li>
             <li className="page-item">
-                <button className="page-link docs-creator" href="#">2</button>
+                <ButtonField className="page-link docs-creator" href="#">2</ButtonField>
             </li>
             <li className="page-item">
                 <ButtonField className="page-link docs-creator" href="#">3</ButtonField>
