cec9b6ad1a493d6065b75bf2da77c10a6dc06963
Muyinda ROgers <moverr@gmail.com>
2021-06-26 11:28:43 +0300

    Debuggggiing

--- src/components/services/ProfileService.js
+++ src/components/services/ProfileService.js
@@ -5,15 +5,13 @@ const url = "https://api.github.com/search/users";
 const Authorization = 'token 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';
 
 function ProfileService(props) {
-  axios.get(`${url}`, {
+  var promise = axios.get(`${url}`, {
     headers: {
       'Accept': 'application/vnd.github.v3+json',
       'Authorization': Authorization
     }
-  }).then((response) => {
-    console.log(response.data);
-  }).catch(error => console.log(error));
-
+  });
+  return promise;
 
 }
 
